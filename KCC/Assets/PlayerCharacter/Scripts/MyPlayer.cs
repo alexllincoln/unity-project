4b02d034c25763fd118594f9e2a5ec9ed5c274e1
alexllincoln <alex.lincoln@gmail.com>
2019-09-26 15:14:44 -0500

    Basic movement and Gravity
    Walkthrough for basic movement and gravity completed. This is where basic player inputs are wired up to move the player character

--- KCC/Assets/PlayerCharacter/Scripts/MyPlayer.cs
+++ KCC/Assets/PlayerCharacter/Scripts/MyPlayer.cs
@@ -13,7 +13,11 @@ namespace PlayerCharacter
         public Transform CameraFollowPoint;
         public MyCharacterController Character;
 
-        private Vector3 _lookInputVector = Vector3.zero;
+        private const string MouseXInput = "Mouse X";
+        private const string MouseYInput = "Mouse Y";
+        private const string MouseScrollInput = "Mouse ScrollWheel";
+        private const string HorizontalInput = "Horizontal";
+        private const string VerticalInput = "Vertical";
 
         private void Start()
         {
@@ -23,7 +27,8 @@ namespace PlayerCharacter
             OrbitCamera.SetFollowTransform(CameraFollowPoint);
 
             // Ignore the character's collider(s) for camera obstruction checks
-            OrbitCamera.IgnoredColliders = Character.GetComponentsInChildren<Collider>().ToList();
+            OrbitCamera.IgnoredColliders.Clear();
+            OrbitCamera.IgnoredColliders.AddRange(Character.GetComponentsInChildren<Collider>());
         }
 
         private void Update()
@@ -34,35 +39,49 @@ namespace PlayerCharacter
             }
 
             HandleCameraInput();
+            HandleCharacterInput();
         }
 
         private void HandleCameraInput()
         {
             // Create the look input vector for the camera
-            float mouseLookAxisUp = Input.GetAxisRaw("Mouse Y");
-            float mouseLookAxisRight = Input.GetAxisRaw("Mouse X");
-            _lookInputVector = new Vector3(mouseLookAxisRight, mouseLookAxisUp, 0f);
+            float mouseLookAxisUp = Input.GetAxisRaw(MouseYInput);
+            float mouseLookAxisRight = Input.GetAxisRaw(MouseXInput);
+            Vector3 lookInputVector = new Vector3(mouseLookAxisRight, mouseLookAxisUp, 0f);
 
             // Prevent moving the camera while the cursor isn't locked
             if (Cursor.lockState != CursorLockMode.Locked)
             {
-                _lookInputVector = Vector3.zero;
+                lookInputVector = Vector3.zero;
             }
 
             // Input for zooming the camera (disabled in WebGL because it can cause problems)
-            float scrollInput = -Input.GetAxis("Mouse ScrollWheel");
+            float scrollInput = -Input.GetAxis(MouseScrollInput);
 #if UNITY_WEBGL
-            scrollInput = 0f;
+        scrollInput = 0f;
 #endif
 
             // Apply inputs to the camera
-            OrbitCamera.UpdateWithInput(Time.deltaTime, scrollInput, _lookInputVector);
+            OrbitCamera.UpdateWithInput(Time.deltaTime, scrollInput, lookInputVector);
 
             // Handle toggling zoom level
             if (Input.GetMouseButtonDown(1))
             {
                 OrbitCamera.TargetDistance = (OrbitCamera.TargetDistance == 0f) ? OrbitCamera.DefaultDistance : 0f;
             }
+        }
+
+        private void HandleCharacterInput()
+        {
+            PlayerCharacterInputs characterInputs = new PlayerCharacterInputs();
+
+            // Build the CharacterInputs struct
+            characterInputs.MoveAxisForward = Input.GetAxisRaw(VerticalInput);
+            characterInputs.MoveAxisRight = Input.GetAxisRaw(HorizontalInput);
+            characterInputs.CameraRotation = OrbitCamera.Transform.rotation;
+
+            // Apply inputs to character
+            Character.SetInputs(ref characterInputs);
         }
     }
 }
